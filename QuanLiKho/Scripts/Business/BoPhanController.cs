a6a98299d38fc4e034549b44e2f34c61791fcd33
xuantung95tt <xuantung95tt@gmail.com>
2016-10-07 21:21:51 +0700

    Update

--- QuanLiKho/Scripts/Business/BoPhanController.cs
+++ QuanLiKho/Scripts/Business/BoPhanController.cs
@@ -1,124 +1,131 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Data;
-using System.Data.SqlClient;
-using Microsoft.ApplicationBlocks.Data;
 
 namespace QuanLiKho
 {
-	public class BoPhanController
-	{
+    public class BoPhanController
+    {
+        #region Constructor
 
-	#region Constructor
-	public BoPhanController()
-	{}
-	#endregion
+        public BoPhanController()
+        { }
 
-	#region Private Variables
-	private int _BPMa;
-	private string _BPTen;
-	private string _BPGhiChu;
-	BoPhan  objclstblBoPhan;
-	#endregion
+        #endregion Constructor
 
-	#region Public Properties
-	public int BPMa
-	{ 
-		get { return _BPMa; }
-		set { _BPMa = value; }
-	}
-	public string BPTen
-	{ 
-		get { return _BPTen; }
-		set { _BPTen = value; }
-	}
-	public string BPGhiChu
-	{ 
-		get { return _BPGhiChu; }
-		set { _BPGhiChu = value; }
-	}
-	#endregion
+        #region Private Variables
 
-	#region Public Methods
-	public DataTable Select()
-	{
-		DataTable dt;
-		try
-		{
-			objclstblBoPhan = new BoPhan();
-			
-			objclstblBoPhan.BPMa = BPMa;
-		
-			dt = objclstblBoPhan.Select();
-			return dt;
-		}
-		catch(Exception ex)
-		{
-			throw new Exception(ex.Message);
-		}
-	}
-	public bool Insert()
-	{
-		try
-		{
-			objclstblBoPhan = new BoPhan();
-			
-			objclstblBoPhan.BPTen = BPTen;
-			objclstblBoPhan.BPGhiChu = BPGhiChu;
-		
-			if(objclstblBoPhan.Insert())
-			{
-				return true;
-			}
-			return false;
-		}
-		catch(Exception ex)
-		{
-			throw new Exception(ex.Message);
-		}
-	}
-	public bool Update()
-	{
-		try
-		{
-			objclstblBoPhan = new BoPhan();
-			
-			objclstblBoPhan.BPMa = BPMa;
-			objclstblBoPhan.BPTen = BPTen;
-			objclstblBoPhan.BPGhiChu = BPGhiChu;
-		
-			if(objclstblBoPhan.Update())
-			{
-				return true;
-			}
-			return false;
-		}
-		catch(Exception ex)
-		{
-			throw new Exception(ex.Message);
-		}
-	}
-	public bool Delete()
-	{
-		try
-		{
-			objclstblBoPhan = new BoPhan();
-			
-			objclstblBoPhan.BPMa = BPMa;
-		
-			if(objclstblBoPhan.Delete())
-			{
-				return true;
-			}
-			return false;
-		}
-		catch(Exception ex)
-		{
-			throw new Exception(ex.Message);
-		}
-	}
-	#endregion
+        private int _BPMa;
+        private string _BPTen;
+        private string _BPGhiChu;
+        private BoPhan objclstblBoPhan;
 
-	}
+        #endregion Private Variables
+
+        #region Public Properties
+
+        public int BPMa
+        {
+            get { return _BPMa; }
+            set { _BPMa = value; }
+        }
+
+        public string BPTen
+        {
+            get { return _BPTen; }
+            set { _BPTen = value; }
+        }
+
+        public string BPGhiChu
+        {
+            get { return _BPGhiChu; }
+            set { _BPGhiChu = value; }
+        }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        public DataTable Select()
+        {
+            DataTable dt;
+            try
+            {
+                objclstblBoPhan = new BoPhan();
+
+                objclstblBoPhan.BPMa = BPMa;
+
+                dt = objclstblBoPhan.Select();
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public bool Insert()
+        {
+            try
+            {
+                objclstblBoPhan = new BoPhan();
+
+                objclstblBoPhan.BPTen = BPTen;
+                objclstblBoPhan.BPGhiChu = BPGhiChu;
+
+                if (objclstblBoPhan.Insert())
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public bool Update()
+        {
+            try
+            {
+                objclstblBoPhan = new BoPhan();
+
+                objclstblBoPhan.BPMa = BPMa;
+                objclstblBoPhan.BPTen = BPTen;
+                objclstblBoPhan.BPGhiChu = BPGhiChu;
+
+                if (objclstblBoPhan.Update())
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public bool Delete()
+        {
+            try
+            {
+                objclstblBoPhan = new BoPhan();
+
+                objclstblBoPhan.BPMa = BPMa;
+
+                if (objclstblBoPhan.Delete())
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        #endregion Public Methods
+    }
 }
